4eab5b8c12ad04e1271c30c991899f90f2d90ff3
KIM <kjl99ra@gmail.com>
2021-04-23 20:57:38 +0900

    refix_again

--- calculator.py
+++ calculator.py
@@ -5,7 +5,6 @@ def add(a,b):
 def subtract(a,b):
 	return a-b
 
-<<<<<<< HEAD
 def divide_free(a,b):
 	return a/b
 
@@ -14,7 +13,4 @@ def multiply(a,b):
 
 def square(a,b):
 	return a*a
-=======
-def multiply(a,b):
-	return a*b
->>>>>>> 5ea569bbe1abd39ec29138d37dbcdc898fe048d1
+
